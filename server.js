28c61022e15d133f3066f5ac69de5a39de4d9f7f
Riza Fahmi <rizafahmi@gmail.com>
2024-05-09 18:13:41 +0700

    Menambahkan fungsi compare dengan Gemini API

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import http from "http";
 import fs from "fs";
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
 function parse_options(options) {
   const topic = options[0].split("=")[1];
@@ -8,8 +13,60 @@ function parse_options(options) {
   return { topic, opt1, opt2 };
 }
 
+async function compare(topic, opt1, opt2) {
+  const MODEL_NAME = "gemini-1.0-pro";
+  const { API_KEY } = process.env;
+  if (!API_KEY) {
+    console.error("Please provide the API Key.");
+    return;
+  }
+
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const parts = [
+    {
+      text: `Please choose your answer given by user delimited by triple dash below and give short reason why. You will answer in Bahasa Indonesia.\n\nExample:\nUser: Mending laptop atau rakit pc?\nAsisten: Mending merakit PC, karena:\n\n* Lebih hemat biaya\n* Lebih fleksibel dalam memilih komponen\n* Dapat di-upgrade dengan mudah\n* Lebih cocok untuk kebutuhan spesifik\n\n\n\n---\n\nUser: ${topic} mending ${opt1} atau ${opt2}?`,
+    },
+  ];
+
+  const result = await model.generateContent({
+    contents: [{ role: "user", parts }],
+    generationConfig,
+    safetySettings,
+  });
+
+  const { response } = result;
+  return response.text();
+}
+
 (async function () {
-  const server = http.createServer((req, res) => {
+  const server = http.createServer(async function (req, res) {
     const { url } = req;
     if (url === "/health") {
       res.writeHead(200).end("OK");
@@ -22,8 +79,9 @@ function parse_options(options) {
       const options = decodeURIComponent(search.substring(1)).split("&");
       const { topic, opt1, opt2 } = parse_options(options);
 
-      console.log({ topic, opt1, opt2 });
-      res.writeHead(200).end(JSON.stringify({ topic, opt1, opt2 }));
+      const suggestion = await compare(topic, opt1, opt2);
+
+      res.writeHead(200).end(suggestion);
     } else {
       console.error(`${url} not found`);
       res.writeHead(404).end("Not Found");
