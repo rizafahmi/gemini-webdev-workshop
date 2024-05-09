bc08cc5c6fc6d872654e880fba11a00b5e9cdf92
Riza Fahmi <rizafahmi@gmail.com>
2024-05-09 17:10:53 +0700

    Backend

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import http from "http";
+import fs from "fs";
 
 (async function () {
   const server = http.createServer((req, res) => {
     const { url } = req;
     if (url === "/health") {
       res.writeHead(200).end("OK");
+    } else if (url === "/" || url === "/index.html") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(fs.readFileSync("./public/index.html"));
     } else {
       console.error(`${url} not found`);
       res.writeHead(404).end("Not Found");
