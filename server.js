7cfb5c8d5117e3e8a283cc9b291f0f2f58cce4f0
Riza Fahmi <rizafahmi@gmail.com>
2024-05-09 17:17:28 +0700

    Backend handle comparation

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import http from "http";
 import fs from "fs";
 
+function parse_options(options) {
+  const topic = options[0].split("=")[1];
+  const opt1 = options[1].split("=")[1];
+  const opt2 = options[2].split("=")[1];
+  return { topic, opt1, opt2 };
+}
+
 (async function () {
   const server = http.createServer((req, res) => {
     const { url } = req;
@@ -9,6 +16,14 @@ import fs from "fs";
     } else if (url === "/" || url === "/index.html") {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(fs.readFileSync("./public/index.html"));
+    } else if (url.startsWith("/compare")) {
+      const parsedUrl = new URL(`http://localhost/${url}`);
+      const { search } = parsedUrl;
+      const options = decodeURIComponent(search.substring(1)).split("&");
+      const { topic, opt1, opt2 } = parse_options(options);
+
+      console.log({ topic, opt1, opt2 });
+      res.writeHead(200).end(JSON.stringify({ topic, opt1, opt2 }));
     } else {
       console.error(`${url} not found`);
       res.writeHead(404).end("Not Found");
